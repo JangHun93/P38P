d03f282107aed899acf157353a741ccb96cae8a2
JangHun93 <pain3992@gmail.com>
2024-06-24 09:05:35 +0900

    First Init

---
+++ Source/P38_Practice/P38_Practice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "P38_Practice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, P38_Practice, "P38_Practice" );
